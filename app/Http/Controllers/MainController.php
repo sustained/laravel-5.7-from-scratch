5029b42d424285702ddbe6260f6779b10928a145
sustained <sustained.dissonance@gmail.com>
2018-11-13 02:32:02 +0000

    Take a short break.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,7 +14,8 @@ class MainController extends Controller
             ['task' => 'Keep learning Postgres', 'completed' => false],
             ['task' => 'Keep learning Redis', 'completed' => false],
             ['task' => 'Keep learning modern HTML/CSS', 'completed' => true],
-            ['task' => 'Stay calm', 'completed' => false]
+            ['task' => 'Stay calm', 'completed' => false],
+            ['task' => 'Take a (tea) break :D', 'completed' => true]
         ];
 
         return view('home', ['tasks' => $tasks]);
